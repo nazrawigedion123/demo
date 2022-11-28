4653106564d8c140c3919c9f89b3398ec4cec510
hp <nazrawigedion23@gmail.com>
2022-11-28 12:08:46 +0300

    i hav commited this

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 print('hi git')
 print('hi git')
+print('hi git')
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
