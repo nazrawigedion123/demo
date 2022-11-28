c86b4a76e38087a9a127d90a46a347c27783b19d
hp <nazrawigedion23@gmail.com>
2022-11-28 11:58:56 +0300

    i hav commited this

--- main.py
+++ main.py
@@ -3,5 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 print('hi git')
+print('hi git')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
